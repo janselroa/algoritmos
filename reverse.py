6d406916adbd65b3492b402fc712ddb7f6624213
Jansel Roa <65916518+janselroa@users.noreply.github.com>
2021-03-12 09:41:22 -0400

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,12 @@
+def reverse(array):
+	longitud=len(array)-1
+	for i in range(0, longitud):
+		temp=array[i]
+		array[i]=array[i+1]
+		array[i+1]=temp
+
+	return array
+
+
+#prueba
+print(reverse([1, 2, 3]))
